796627349ee8f2bbb6efba07c67091e94ce3fc39
crocell1116 <seokmin111682@gmail.com>
2025-11-23 08:32:55 +0900

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const BluetoothLedPage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const BluetoothLedPage(),
+    );
   }
 }
